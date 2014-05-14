709f9f18d7702ab97e9a350c5ffe74eb3edd681a
Christopher Gerber <chriswgerber@gmail.com>
2014-05-14 13:46:59 -0500

    Adds link to support page

--- wp-content/themes/responsive/templates/page_support-request.php
+++ wp-content/themes/responsive/templates/page_support-request.php
@@ -8,6 +8,10 @@ get_header(); ?>
 
     <div id="content" class="grid col-940">
 
+        <p>
+            <a href="https://trello.com/b/MNr7er6C" target="_blank">Web Help desk</a>
+        </p>
+
         <?php if (empty($_POST)) {
 
             include ( get_template_directory() . '/templates/support-form.php' );
